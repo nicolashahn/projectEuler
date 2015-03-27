b05a454b4cefb4c6a1513f4140257feccd5720ff
nicolashahn <nhahn@ucsc.edu>
2015-03-26 23:53:22 -0700

    p6 done

--- problem6.py
+++ problem6.py
@@ -2,4 +2,16 @@
 # Nicolas Hahn
 # Problem 6
 
-for i in range(1,100):
+def sumOfSquares(n):
+	result = 0
+	for i in range(1,n+1):
+		result += i*i
+	return result
+
+def squareOfSum(n):
+	result = 0
+	for i in range(1,n+1):
+		result += i
+	return result*result
+
+print squareOfSum(100)-sumOfSquares(100)
